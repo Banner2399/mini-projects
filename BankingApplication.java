78434036dcf2d87805d18fefaa38bf33dc06edab
Banner2399 <118509824+Banner2399@users.noreply.github.com>
2023-04-24 21:12:40 +0530

    Delete BankingApplication.java

--- BankingApplication.java
+++
@@ -1,9 +0,0 @@
-package BankingApplication;
-
-public class BankingApplication {
-    public static void main(String[] args){
-
-        Account person=new Account("Banner" , "109255");
-        person.display();
-    }
-}
